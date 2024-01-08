9eedffc51520a4368306a3d419a9362176bcf903
saeidehmir <125218039+saeidehmir@users.noreply.github.com>
2024-01-07 23:26:51 -0500

    renaming the code

--- test_my_package.py
+++ test_my_package.py
@@ -7,6 +7,10 @@ class TestGetTopRepos(unittest.TestCase):
     def test_get_top_repos_returns_data(self):
         """Test that the function returns a list of tuples with the correct length."""
         N = 5
-        result = get_top_repos(N)
+        result = get_top_repos(N, test=True)
         self.assertIsInstance(result, list)
         self.assertEqual(len(result), N)
+
+
+if __name__ == '__main__':
+    unittest.main()
