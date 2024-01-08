796f9cd91cb262f2cd9bb353495b0e6d8a6300ab
saeidehmir <125218039+saeidehmir@users.noreply.github.com>
2024-01-07 23:03:13 -0500

    Create unittest.py

---
+++ unittest.py
@@ -0,0 +1,12 @@
+# tests.py
+import unittest
+from TopGithubRepos.get_top_repos import get_top_repos
+
+class TestGetTopRepos(unittest.TestCase):
+
+    def test_get_top_repos_returns_data(self):
+        """Test that the function returns a list of tuples with the correct length."""
+        N = 5
+        result = get_top_repos(N)
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), N)
